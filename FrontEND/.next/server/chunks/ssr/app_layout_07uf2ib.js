9e7a0f0599c6369bf116edc99f15a7915bba4f2f
lAllireza <zare40130@tutamail.com>
2026-08-19 22:48:35 +0330

    ...

---
+++ FrontEND/.next/server/chunks/ssr/app_layout_07uf2ib.js
@@ -0,0 +1,3 @@
+module.exports=[62925,a=>{"use strict";var b=a.i(7997);a.s(["default",0,function({children:a}){return(0,b.jsx)("html",{lang:"fa",dir:"rtl",children:(0,b.jsx)("body",{style:{margin:0,padding:0,backgroundColor:"#f4f4f9"},children:a})})},"metadata",0,{title:"NextJS Docker Test",description:"Testing Docker Containers"}])},19325,a=>{a.n(a.i(62925))}];
+
+//# sourceMappingURL=app_layout_07uf2ib.js.map
